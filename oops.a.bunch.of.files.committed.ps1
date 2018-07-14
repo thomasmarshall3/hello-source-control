997da72610f079f581c8b3eb6d4c63ccaf4d8cab
Thomas Marshall <thomas.marshall@live.com>
2018-07-14 13:58:04 -0400

    Adding files 2, 3, 4, 5, 6, oops...

--- oops.a.bunch.of.files.committed.ps1
+++ oops.a.bunch.of.files.committed.ps1
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 ﻿ for ($d=7; $d -le 12;$d++) {
+=======
+﻿ for ($d=1; $d -le 6;$d++) {
+>>>>>>> 03cb59b... Adding files 2, 3, 4, 5, 6, oops...
    New-Item file$d.md;
    git add file$d.md;
    git commit -m "adding file$d.md";
