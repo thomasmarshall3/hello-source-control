721aa93f3f799600f63977052dbad1bad5674566
Thomas Marshall <thomas.marshall@live.com>
2018-07-14 14:31:11 -0400

    remessaging example

--- oops.a.bunch.of.files.committed.ps1
+++ oops.a.bunch.of.files.committed.ps1
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿ for ($d=7; $d -le 12;$d++) {
 =======
 ﻿ for ($d=1; $d -le 6;$d++) {
 >>>>>>> 03cb59b... Adding files 2, 3, 4, 5, 6, oops...
+=======
+﻿ for ($d=7; $d -le 12;$d++) {
+>>>>>>> 57e4003... remessaging example
    New-Item file$d.md;
    git add file$d.md;
    git commit -m "adding file$d.md";
